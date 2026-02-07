e145cd9c33f19de334399bf809411fd8889ed986
Aman Toumaj <tomajexpress@gmail.com>
2026-02-07 22:57:49 +0100

    cleaning code in program.cs

--- StrategyDesignPattern.ConsoleApp/Program.cs
+++ StrategyDesignPattern.ConsoleApp/Program.cs
@@ -1,18 +1,17 @@
-﻿// 1. Initialize the Container
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using StrategyDesignPattern.Core;
 using StrategyDesignPattern.Core.Configurations;
 using StrategyDesignPattern.Core.Enums;
 
 var services = new ServiceCollection();
 
-// 2. Register Services
+// 1. Register Services
 DependencyConfigurations.RegisterTaxCaculation(services);
 
-// 3. Build the Provider
+// 2. Build the Provider
 using var serviceProvider = services.BuildServiceProvider();
 
-// 4. Resolve Services
+// 3. Resolve Services
 var salaryCalculator = serviceProvider.GetRequiredService<ISalaryCalculator>();
 
 decimal grossSalary = 5000m;
